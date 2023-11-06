20846a27adbe791ca5a78d36a3582d7a66930d8e
RomanChirkov2017 <138663746+RomanChirkov2017@users.noreply.github.com>
2023-11-06 19:41:00 +1000

    Добавлен файл json_saver

---
+++ src/json_saver.py
@@ -0,0 +1,41 @@
+import json
+
+from src.abstract_classes import FileSaver
+from src.vacancy import Vacancy
+
+class JsonSaver(FileSaver):
+    """Класс для работы с json-файлом."""
+
+    def __init__(self, keyword, vacancies_json) -> None:
+        """Инициализатор класса JsonSaver"""
+        self.keyword = keyword
+        self.filename = "vacancies_JSON.json"
+        self.save_data(vacancies_json)
+        self.vacancy_list = []
+
+    def save_data(self, vacancies_json) -> None:
+        """Запись данных в json-файл."""
+        with open(self.filename, "w", encoding="UTF-8") as file:
+            json.dump(vacancies_json, file, indent=4, ensure_ascii=False)
+
+    def add_vacancy(self) -> list:
+        """Добавляет вакансии в список vacancy_list."""
+        with open(self.filename, "r") as file:
+            vacancies = json.load(file)
+
+        for vacancy in vacancies:
+            new_vacancy = Vacancy(
+                vacancy['name'],
+                vacancy['url'],
+                vacancy['salary_from'],
+                vacancy['salary_to'],
+                vacancy['currency'],
+                vacancy['description'],
+            )
+            self.vacancy_list.append(new_vacancy)
+        return self.vacancy_list
+
+    def del_vacancy(self) -> None:
+        """Удаляет данные о вакансиях из json-файла."""
+        with open(self.filename, "w") as file:
+            json.dump("", file, indent=4, ensure_ascii=False)
